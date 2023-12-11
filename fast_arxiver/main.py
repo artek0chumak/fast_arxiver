499e8ec04dc2a3cd65d36e6c058b5358de181d7a
Artem Chumachenko <artek.chumak@gmail.com>
2023-12-11 15:23:46 +0400

    Add ollama to suggest keywords

--- fast_arxiver/main.py
+++ fast_arxiver/main.py
@@ -1,10 +1,9 @@
 import os
 
-import arxiv
 import typer
 from rich.console import Console
 
-from fast_arxiver.utils import markdown_template, url_to_id
+from fast_arxiver.utils import markdown_template, get_article, get_ollama_response
 
 app = typer.Typer(help="Archive page for arxiv.org")
 console = Console()
@@ -14,12 +13,10 @@ console = Console()
 def main(
     arxiv_url: str,
     path_to_archive_folder: str = "~/Documents/Main Obsidian/Research/Papers/",
+    dont_suggest_keywords: bool = False,
 ):
-    article_id = url_to_id(arxiv_url)
-    arxiv_result = arxiv.Search(id_list=[article_id])
-    results = list(arxiv_result.results())
-    result = results[0]
-    console.print(f"Found {len(results)} articles. Will use first one to archive.")
+    result = get_article(arxiv_url)
+    console.print(f'Found "{result.title}" article.')
     file_text = markdown_template.format(
         date=result.published.strftime("%Y-%m"),
         authors=", ".join(f"[[{a.name}]]" for a in result.authors),
@@ -34,6 +31,23 @@ def main(
         with open(file_path, "w") as result_file:
             result_file.write(file_text)
         console.print(f"All done. {file_name} have been archived!")
+
+    if not dont_suggest_keywords:
+        suggest_keywords_for_article(arxiv_url)
+
+
+@app.command()
+def suggest_keywords_for_article(
+    arxiv_url: str,
+    path_to_key_items_folder: str = "~/Documents/Main Obsidian/KeyItems/",
+):
+    result = get_article(arxiv_url)
+    key_items = list()
+    for file in os.listdir(os.path.expanduser(path_to_key_items_folder)):
+        if file.endswith(".md"):
+            key_items.append(file[:-3])
+    result = get_ollama_response(result.title, result.summary, key_items)
+    console.print(f"Ollama suggestions for keywords: {result}")
 
 
 if __name__ == "__main__":
--- fast_arxiver/utils.py
+++ fast_arxiver/utils.py
@@ -1,3 +1,11 @@
+import arxiv
+import httpx
+import logging
+import random
+
+from typing import Optional
+
+
 markdown_template = """---
 publication_date: {date}
 status: Waiting
@@ -10,6 +18,15 @@ key_items::
 ## Abstract
 {summary}
 """
+
+
+keywords_prompt = """There are all of the keywords:
+{keywords}
+
+Read article summary and suggest top 10 keywords for it:
+{title}
+{summary}
+Keywords:"""
 
 
 def url_to_id(url: str) -> str:
@@ -27,3 +44,43 @@ def url_to_id(url: str) -> str:
         url = url[:-4]
 
     return url.split("/")[-1]
+
+
+def get_article(arxiv_url: str) -> arxiv.Result:
+    article_id = url_to_id(arxiv_url)
+    arxiv_result = arxiv.Search(id_list=[article_id])
+    results = list(arxiv_result.results())
+    return results[0]
+
+
+def get_ollama_response(
+    article_title: str,
+    article_summary: str,
+    keywords: list[str],
+    ollama_url: str = "http://localhost:11434",
+    model_name: Optional[str] = None,
+    max_model_size: int = 1e10,
+) -> str:
+    prompt = keywords_prompt.format(
+        title=article_title,
+        summary=article_summary,
+        keywords="\n".join(keywords),
+    )
+    if model_name is None:
+        local_models = httpx.get(ollama_url + "/api/tags").json()["models"]
+        if len(local_models) == 0:
+            raise ValueError(
+                "Cannot find any local model. Please, pull one using terminal."
+            )
+        model_size = max_model_size
+        for model_info in local_models:
+            if model_size > model_info.get("size", max_model_size):
+                model_size = model_info.get("size")
+                model_name = model_info["name"]
+
+    response = httpx.post(
+        ollama_url + "/api/generate",
+        json={"model": model_name, "prompt": prompt, "stream": False},
+        timeout=120,
+    )
+    return response.json()["response"]
